8329ec8b018a2cf2f033fd8c2f4420241ce6a1cc
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-23 12:34:09 -0600

    Inference and predictions

--- main.py
+++ main.py
@@ -163,3 +163,62 @@ plt.xlabel("epoch")
 plt.show()
 plt.savefig("figure04.png")
 plt.close()
+
+# Loading the Colormap
+colormap = loadmat(
+    "/home/est_posgrado_manuel.suarez/data/instance-level_human_parsing/instance-level_human_parsing/human_colormap.mat"
+)["colormap"]
+colormap = colormap * 100
+colormap = colormap.astype(np.uint8)
+
+def infer(model, image_tensor):
+    predictions = model.predict(np.expand_dims((image_tensor), axis=0))
+    predictions = np.squeeze(predictions)
+    predictions = np.argmax(predictions, axis=2)
+    return predictions
+
+def decode_segmentation_masks(mask, colormap, n_classes):
+    r = np.zeros_like(mask).astype(np.uint8)
+    g = np.zeros_like(mask).astype(np.uint8)
+    b = np.zeros_like(mask).astype(np.uint8)
+    for l in range(0, n_classes):
+        idx = mask == 1
+        r[idx] = colormap[l, 0]
+        g[idx] = colormap[l, 1]
+        b[idx] = colormap[l, 2]
+    rgb = np.stack([r, g, b], axis=2)
+    return rgb
+
+def get_overlay(image, colored_mask):
+    image = tf.keras.utils.array_to_img(image)
+    image = np.array(image).astype(np.uint8)
+    overlay = cv2.addWeighted(image, 0.35, colored_mask, 0.65, 0)
+    return overlay
+
+def plot_samples_matplotlib(display_list, filename, figsize=(5, 3)):
+    _, axes = plt.subplots(nrows=1, ncols=len(display_list), figsize=figsize)
+    for i in range(len(display_list)):
+        if display_list[i].shape[-1] == 3:
+            axes[i].imshow(tf.keras.utils.array_to_img(display_list[i]))
+        else:
+            axes[i].imshow(display_list[i])
+    plt.show()
+    plt.savefig(filename)
+    plt.close()
+
+def plot_predictions(images_list, colormap, suffix, model):
+    for idx, image_file in enumerate(images_list):
+        image_tensor = read_image(image_file)
+        prediction_mask = infer(image_tensor=image_tensor, model=model)
+        prediction_colormap = decode_segmentation_masks(prediction_mask, colormap, 20)
+        overlay = get_overlay(image_tensor, prediction_colormap)
+        plot_samples_matplotlib(
+            [image_tensor, overlay, prediction_colormap],
+            f"prediction0{idx}_{suffix}.png",
+            figsize=(18, 14)
+        )
+
+# Inference on Train Images
+plot_predictions(train_images[:4], colormap, "train", model=model)
+# Inference on Validation Images
+plot_predictions(val_images[:4], colormap, "valid", model=model)
