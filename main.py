03ef3cef21ed99098523b1e260208d38580de0e2
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-23 11:10:51 -0600

    Initial setup

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+import cv2
+import numpy as np
+from glob import glob
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
