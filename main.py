2358f5ee51ac216824a64bce8d4c4360b9ac8cd8
Arturo Suárez <manuel.suarez@cimat.mx>
2023-09-23 11:24:56 -0600

    Configuring dataset path

--- main.py
+++ main.py
@@ -8,3 +8,19 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+
+IMAGE_SIZE = 512
+BATCH_SIZE = 4
+NUM_CLASSES = 20
+DATA_DIR = "/home/est_posgrado_manuel.suarez/data/instance-leve_human_parsing/instance-level_human_parsing/Trainig"
+NUM_TRAIN_IMAGES = 1000
+NUM_VAL_IMAGES = 50
+
+train_images = sorted(glob(os.path.join(DATA_DIR, "Images/*")))[:NUM_TRAIN_IMAGES]
+train_masks = sorted(glob(os.path.join(DATA_DIR, "Category_ids/*")))[:NUM_TRAIN_IMAGES]
+val_images = sorted(glob(os.path.join(DATA_DIR, "Images/*")))[
+    NUM_TRAIN_IMAGES : NUM_VAL_IMAGES + NUM_TRAIN_IMAGES
+]
+val_masks = sorted(glob(os.path.join(DATA_DIR, "Category_ids/*")))[
+    NUM_TRAIN_IMAGES : NUM_VAL_IMAGES + NUM_TRAIN_IMAGES
+]
